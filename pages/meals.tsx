976cd10c072ca0c80429c369dd671ec675facb75
robin <80066142+robinnlmn@users.noreply.github.com>
2022-03-29 13:08:24 +0200

    responsive done and deploy to vercel

---
+++ pages/meals.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { useEffect } from 'react'
+import styles from '../styles/Home.module.css'
+
+const Meals: NextPage = () => {
+    return (
+        <div className={styles.container}>
+            <Head>
+            </Head>
+
+            <main className={styles.main}>
+
+            </main>
+
+            {/* <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer> */}
+        </div>
+    )
+}
+
+export default Meals
